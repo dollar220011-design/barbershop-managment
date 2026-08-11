98c19f0955cafd35cfce03ae0bf90e49a8d932d4
dollar220011-design <dollar220011@gmail.com>
2026-08-12 03:58:47 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'src',
+
   plugins: [react()],
+
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     emptyOutDir: true,
   },
 });
